96db519f321e055836e7802f7e6dc96b987ec5ea
akdiam <adamkim@umich.edu>
2023-03-21 21:35:56 -0400

    Add button to event box

--- components/events/EventList.js
+++ components/events/EventList.js
@@ -12,7 +12,7 @@ export default function EventList({ events }) {
   return (
     <div className="text-black mx-auto w-full py-2 lg:py-6">
       {events.map((event, _) => (
-        <button className="my-2 px-4 lg:px-8 w-full" key={event.title}>
+        <div className="my-2 px-4 lg:px-8 w-full" key={event.title}>
           <div className="shadow-xl border border-slate-700 rounded-md w-full bg-secondary hover:bg-secondaryHover transition duration-150 ease-in-out min-h-52 lg:h-72 text-left px-4 py-3 flex flex-col justify-around">
             <div className="text-xl lg:text-2xl text-white font-semibold tracking-wide pb-2">
               {event.title}
@@ -30,9 +30,16 @@ export default function EventList({ events }) {
               <div className="text-xs lg:text-sm font-thin text-gray-400">
                 {event?.events?.eventType === 'Virtual' && event?.events?.eventType}
               </div>
+              {event?.events?.callToActionLink && 
+              <a href={event?.events?.callToActionLink}>
+                <button className='bg-yellow-300 hover:bg-yellow-400 text-black rounded-md py-2 px-4 mt-3'>
+                  {event?.events?.callToActionButtonText}
+                </button>
+              </a>
+              }
             </div>
           </div>
-        </button>
+        </div>
       ))}
     </div>
   );
